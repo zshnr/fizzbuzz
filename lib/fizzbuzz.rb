fb736745a4a007a763278f3a30bd3ea3866d837a
Zeeshan Rasool <zrasool88@gmail.com>
2014-09-22 12:21:33 +0100

    Second FizzBuzz Commit

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,14 @@
 class FizzBuzz
 
-def is_divisible_by_three?(number)
-	true
-end
+	def is_divisible_by_three?(number)
+		number % 3 == 0
+	end
 
+	def is_divisible_by_five?(number)
+		number % 5 == 0	
+	end
+
+	def is_divisible_by_fifteen?(number)
+		number % 15 == 0
+	end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,8 +1,18 @@
 require 'FizzBuzz'
 describe FizzBuzz  do
 	
+	let(:fizzbuzz) { FizzBuzz.new }
+
 	it "knows when a number is divisible by 3" do
-		fizzbuzz = FizzBuzz.new
 		expect(fizzbuzz.is_divisible_by_three?(3)).to be true
 	end
+
+	it "knows when a number is divisible by 5" do
+		expect(fizzbuzz.is_divisible_by_five?(5)).to be true
+	end
+
+	it "knows when a number is divisible by 15" do
+		expect(fizzbuzz.is_divisible_by_fifteen?(15)).to be true
+	end
+
 end
